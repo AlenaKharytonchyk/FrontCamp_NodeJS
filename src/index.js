8ed1f02ff630688573fefe89fa31ebcef94fa808
Alena Kharytonchyk <alena.kharytonchyk@outlook.com>
2019-11-22 15:59:33 +0300

    feat: done changes in package.json

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('hello from Node.js');
+const express = require('express');
+const app = express();
+
+const url = `https://newsapi.org/v2/sources?apiKey=1d8434c04862439692cc773aa6bfc026`;
+
+app.get(url, function (req, res) {
+  res.json();
+});
+
+app.listen(3000);

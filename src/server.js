f8551288ff910ba7da3a38dc2753e9c52b7d2804
Alena Kharytonchyk <alena.kharytonchyk@outlook.com>
2019-11-25 10:30:01 +0300

    feat: add  logger to file

--- src/server.js
+++ src/server.js
@@ -10,7 +10,8 @@ server.use(body_parser.json());
 
 server.use(expressWinston.logger({
   transports: [
-    new winston.transports.Console()
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: 'combined.log' })
   ],
   format: winston.format.combine(
     winston.format.colorize(),
